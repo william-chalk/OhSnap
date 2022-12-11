f7043e00f9912abce69b61900f39228c03d365f0
William Chalk <3halk1@gmail.com>
2022-12-10 19:25:03 -0500

    added Gallery Props

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,40 +1,37 @@
-import React from "react";
-import Nav from "..";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+// __tests__/Nav.test.js with hard coded categories
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
 
 afterEach(cleanup);
 
-describe("Nav component", () => {
-  //baseline test
-  it("renders", () => {
+describe('Nav component', () => {
+  it('renders', () => {
     render(<Nav />);
   });
-  //snapshot test
-  it("matches snapshot", () => {
+
+  it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
-    //assert value comparision
+
     expect(asFragment()).toMatchSnapshot();
   });
-});
+})
 
-describe("emoji is visible", () => {
-  it("inserts emoji into the h2", () => {
+describe('emoji is visible', () => {
+  it('inserts emoji into the h2', () => {
     const { getByLabelText } = render(<Nav />);
 
-    // eslint-disable-next-line testing-library/prefer-screen-queries
-    expect(getByLabelText("camera")).toHaveTextContent("📸");
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
-});
+})
 
-describe("links are visable", () => {
-  it("inserts text into the links", () => {
-    //Arrange
+describe('links are visible', () => {
+  it('inserts text into the links', () => {
     const { getByTestId } = render(<Nav />);
-    //Assert
-    // eslint-disable-next-line testing-library/prefer-screen-queries
-    expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
-    // eslint-disable-next-line testing-library/prefer-screen-queries
-    expect(getByTestId("about")).toHaveTextContent("About Me");
+
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
   });
-});
+
+})
